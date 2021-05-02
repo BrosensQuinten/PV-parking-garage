960fa3b21a944cd0f6d564fe8b5cd0fcd1987e6a
BrosensQuinten <quinten.brosens@student.kuleuven.be>
2021-05-02 16:04:05 +0200

    belpex added

--- matlab/main.m
+++ matlab/main.m
@@ -24,7 +24,7 @@ fixed_installation_cost = 47000;
 marginal_installation_cost = 4000;
 fast_charger_cost = 10000;
 slow_charger_cost = 644; 
-fast_charging_aandeel = xx;
+fast_charging_aandeel = 0;
 slow_charging_aandeel = (1-fast_charging_aandeel);
 fast_chargers = fast_charging_aandeel*aantal_autos;
 slow_chargers = slow_charging_aandeel*aantal_autos;
@@ -39,15 +39,22 @@ ev_charge = 673; %total charge needed per day for total ev fleet in kwh
 ev_charge_initial = 1846; %initial charge of total ev fleet at the start of the day in kwh
 
 %%
+%% belpex waarden formatteren
+Belpex = reshape(xlsread('BelpexFilter.xlsx'),24,[]);
+Belpex = Belpex(9:17,:);
+%%
 
-for i = 1:size(date,1)
-dam_prices(:,i) = load(fullfile(pwd, "day ahead market prices", date(i))).dam ; %day ahead market prices
-dam_9_5(:,i) = dam_prices(10:18,i); %day ahead market prices from 9am till 5pm.
+for i = 1:size(Belpex,2)
+%dam_prices(:,i) = load(fullfile(pwd, "day ahead market prices", date(i))).dam ; %day ahead market prices
+%dam_9_5(:,i) = dam_prices(10:18,i); %day ahead market prices from 9am till 5pm.
 
-[x(i,:), fval(i,1)] = loadProfile(dam_9_5(:,i), ev_charge, max_charging_cap,ev_charge_initial);
+[x(i,:), fval(i,1)] = loadProfile(Belpex(:,i), ev_charge, max_charging_cap,ev_charge_initial);
 
 end
 
+%% battery state of charge
+%for i = 1: size(
+   
 %%
 x_axis = [9 10 11 12 13 14 15 16 17];
 figure
