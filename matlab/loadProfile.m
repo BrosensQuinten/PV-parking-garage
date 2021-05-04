d8220626b037c35019bd0103ef3d9d0c2444a1fd
BrosensQuinten <quinten.brosens@student.kuleuven.be>
2021-05-04 10:48:35 +0200

    debugged load profile

--- matlab/loadProfile.m
+++ matlab/loadProfile.m
@@ -1,4 +1,4 @@
-function [x,fval] = loadProfile(dam_9_5,ev_charge, max_charging_cap,ev_charge_initial)
+function [x,fval] = loadProfile(dam_9_5,ev_charge_max, max_charging_cap,ev_charge_initial)
 %this function minimizes the costs of charging over a 9am to 5pm period.
 %dam_9_5 = prices for electricity
 %ev_charge = total energy required to charge all ev's over period
@@ -12,11 +12,11 @@ f= dam_9_5;
 A = vertcat(-toeplitz(ones(1,len),zeros(1,len)),toeplitz(ones(1,len),zeros(1,len)));
 
 b_initial(1:len,1) = ev_charge_initial; %to allow for discharging of the initial battery energy
-b_charge_max(1:len,1) = ev_charge; % to make sure the battery doesnt get charged above max capacity
+b_charge_max(1:len,1) = ev_charge_max - ev_charge_initial; % to make sure the battery doesnt get charged above max capacity
 b = vertcat(b_initial,b_charge_max);
 
 Aeq= ones(1,len);
-beq= ev_charge;
+beq= ev_charge_max- ev_charge_initial;
 
 lb(1,1:len) = -max_charging_cap;
 ub(1,1:len) = max_charging_cap;
