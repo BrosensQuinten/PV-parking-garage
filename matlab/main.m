5a315257c403b49b264d5fb0f11496c27c580d1c
merijnvd <merijnvandeyck@hotmail.com>
2021-05-04 09:24:33 +0200

    NPV

--- matlab/main.m
+++ matlab/main.m
@@ -6,6 +6,8 @@ Av_commute = 23; %km
 jaarlijkse_afstand = 15000; %km/auto
 aantal_autos = 500; %nog aan te passen 
 lifetime = 4; %gebruikstijd van de bedrijfswagen
+lifetime_chargers = 25;
+time_horizon = lifetime_chargers;
 discount_rate = 0.02; %nog opzoeken wat de discount rate hiervoor is
 %Nissan leaf data
 battery_capacity = 36; %kWh
@@ -24,11 +26,11 @@ fixed_installation_cost = 47000;
 marginal_installation_cost = 4000;
 fast_charger_cost = 10000;
 slow_charger_cost = 644; 
-fast_charging_aandeel = 0;
+fast_charging_aandeel = 1;
 slow_charging_aandeel = (1-fast_charging_aandeel);
 fast_chargers = fast_charging_aandeel*aantal_autos;
 slow_chargers = slow_charging_aandeel*aantal_autos;
-Charger_cost = fixed_installation_cost + marginal_installation_cost*aantal_autos + fast_charger_cost*fast_chargers + slow_charger_cost*slow_chargers;
+charger_cost = fixed_installation_cost + marginal_installation_cost*aantal_autos + fast_charger_cost*fast_chargers + slow_charger_cost*slow_chargers;
 fast_charger_capacity = 50; %kW/paal
 slow_charger_capacity = 7.4; %kW/paal
 max_charging_cap = fast_chargers*fast_charger_capacity + slow_chargers*slow_charger_capacity;
@@ -36,7 +38,7 @@ max_charging_cap = fast_chargers*fast_charger_capacity + slow_chargers*slow_char
 Citroen_kost = 25903; %aankoopprijs citroen c4
 Citroen_resale = 11785; % resale value na 4 jaar
 Citroen_verbruik = 5.52/100; %l/km
-Diesel_prijs = 0.082; 
+Diesel_prijs = 1.491; %euro per liter 
 Diesel_per_jaar = jaarlijkse_afstand *Citroen_verbruik; %l per jaar
 Citroen_fuel_cost = Diesel_per_jaar * Diesel_prijs; %fuel cost per jaar
 Citroen_maintanance = 500; %euro per jaar BRON ZOEKEN 
@@ -44,7 +46,8 @@ Citroen_maintanance = 500; %euro per jaar BRON ZOEKEN
 %% Waarden voor chargen
 ev_charge = laadbeurten*battery_actual*aantal_autos/365; %total charge needed per day for total ev fleet in kwh
 %max_charging_cap = 5*1000; %max charging capacity in kw
-ev_charge_initial = 1846; %initial charge of total ev fleet at the start of the day in kwh
+ev_charge_initial = 1846; %DIT MOET NOG AANGEPAST WORDEN MAAR IK WEET NIET MEER HOE WE DAT BEREKEND HEBBEN
+                          %initial charge of total ev fleet at the start of the day in kwh
 
 %%
 %% belpex waarden formatteren
@@ -64,11 +67,25 @@ end
 %for i = 1: size(
    
 %%
-x_axis = [9 10 11 12 13 14 15 16 17];
-figure
-for i = 1:size(date,1)
-    subplot(2,2,i)
-    plot(x_axis, x(i,:),x_axis,10*dam_9_5(:,i));
-    title(date(i))
+%x_axis = [9 10 11 12 13 14 15 16 17];
+%figure
+%for i = 1:size(date,1)
+%    subplot(2,2,i)
+%    plot(x_axis, x(i,:),x_axis,10*dam_9_5(:,i));
+%    title(date(i))
+%end
+electricity_cost = sum(fval); %kost voor elektriciteit per jaar
+capex = aantal_autos*(Nissan_cost - Citroen_kost) + charger_cost; %additional investment cost
+opex_ev = electricity_cost;
+opex_diesel = Citroen_fuel_cost+Citroen_maintanance;
+savings = opex_diesel-opex_ev;
+NPV = - capex;
+for i=1:time_horizon
+    NPV = NPV + savings/(1+discount_rate)^i;
+    if rem(i,4) == 0
+        NPV = NPV - aantal_autos*(Nissan_cost-Citroen_kost - (Nissan_resale-Citroen_resale) )/(1+discount_rate)^i;
+    end
 end
 
+      
+   
